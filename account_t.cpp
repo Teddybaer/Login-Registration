743107387426718fb8652a8affa2735ef9bcff3f
Teddybaer <kofi.offei14@gmail.com>
2015-01-15 15:53:07 +0100

    Create account_t.cpp

---
+++ account_t.cpp
@@ -0,0 +1,60 @@
+#include "Account.h"
+#include <iostream>
+
+Account::Account(const std::string& usersfile) : filename(usersfile)
+{
+	userdatei.open(filename);
+	if (!userdatei)
+	{
+		std::ofstream userd(filename);
+		userd.close();
+	}
+
+	Admins.insert({ "Teddybaer", "test123" });
+
+	LeseUserfile();
+}
+
+Account::~Account()
+{
+	userdatei.clear();
+	userdatei.trunc;
+
+	AktuallisiereUser();
+
+	userdatei.close();
+}
+
+void Account::AktuallisiereUser()
+{
+	for (auto UserMap : Users)
+	{
+		userdatei << UserMap.first << " " << UserMap.second << std::endl;
+	}
+
+}
+
+void Account::LeseUserfile()
+{
+		std::string id, pw, zeile;
+		std::stringstream openfilezeile;
+
+		while (getline(userdatei, zeile))
+		{
+			openfilezeile.str(zeile);
+			openfilezeile >> id >> pw;
+
+			Users.insert({ id, pw });
+		}
+}
+
+void Account::Add_Account(const std::string username, const std::string password)
+{
+	Users.insert({ username, password });
+}
+
+void Account::Remove_Account(const std::string& username)
+{
+	Users.erase(username);
+}
+
